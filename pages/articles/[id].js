d7e01026b4c367e1fed5b217b26ac008ff7aacf9
Ben Stanfield <bnstnfld@gmail.com>
2020-08-03 08:47:25 -0700

    fix capital

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../../components/layout'
+import Layout from '../../components/Layout'
 import Head from 'next/head'
 import { getAllArticleIds, getArticleData } from '../../lib/articles'
 import Date from '../../components/Date'
