cb7a32401a707b8d60382715968d08635e15f8a2
Ben Stanfield <bnstnfld@gmail.com>
2020-08-03 08:40:58 -0700

    init commit

---
+++ pages/articles/[id].js
@@ -0,0 +1,40 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import { getAllArticleIds, getArticleData } from '../../lib/articles'
+import Date from '../../components/Date'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Article({ article }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{article.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{article.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={article.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: article.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllArticleIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const article = await getArticleData(params.id)
+  return {
+    props: {
+      article
+    }
+  }
+}
